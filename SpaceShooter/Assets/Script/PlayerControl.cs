0ce4557d3b1d6c9e12f274bd70585d1cdf33f337
Dhyego Martins <dfam92@hotmail.com>
2021-04-07 20:49:56 -0300

    Adding Movement To Player

--- SpaceShooter/Assets/Script/PlayerControl.cs
+++ SpaceShooter/Assets/Script/PlayerControl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    public Rigidbody2D playerRb;
 
     // Start is called before the first frame update
     void Start()
@@ -12,8 +14,14 @@ public class PlayerControl : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        playerRb.AddForce(Vector2.right * speed * horizontalInput);
     }
 }
